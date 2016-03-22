b00610caabefe20725cc3348d7ed1329f83ffc8f
Ajay Benno <abenno@andrew.cmu.edu>
2016-03-21 22:29:37 -0400

    Almost have storage working

--- popup.js
+++ popup.js
@@ -8,6 +8,9 @@
 * @param {function(string)} callback - called when the URL of the current tab
 *   is found.
 */
+$(document).ready(function(){
+  $(".dropdown-button").dropdown();
+}); //Activates Dropdown
 function getCurrentTabUrl(callback) {
 // Query filter to be passed to chrome.tabs.query - see
 // https://developer.chrome.com/extensions/tabs#method-query
@@ -17,7 +20,7 @@ var queryInfo = {
 };
 
 chrome.tabs.query(queryInfo, function(tabs) {
-  
+
   var tab = tabs[0];
   var url = tab.url;
   console.assert(typeof url == 'string', 'tab.url should be a string');
@@ -28,7 +31,49 @@ chrome.tabs.query(queryInfo, function(tabs) {
 function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
+//This function handles adding to a dictionary and checks if already
+//already inside the dictionary.
+function addToDictionaryStorage(url) {
 
+  chrome.storage.sync.get(null,function(items){
+    if(jQuery.isEmptyObject(items)){
+      console.log("empty of items");
+      var arr = [];      
+      chrome.storage.sync.set({"data": arr});
+    }
+
+    chrome.storage.sync.get("data",function(arr){
+      if(jQuery.inArray(url,arr["data"]) == -1){
+        console.log(arr["data"]);
+        console.log(url);
+        arr["data"].push(url);
+
+        chrome.storage.sync.set({"data":arr["data"]});
+      }
+    });
+  });
+}
+
+
+function getFromDictonaryStorage(){  
+  chrome.storage.sync.get(null,function(items){
+    if(jQuery.isEmptyObject(items) == false){
+      chrome.storage.sync.get("data",function(arr){
+        var dataElem = arr["data"];
+        var dropd = document.getElementById('collectionID');
+
+        for(var i = 0; i < arr["data"].length;i++){
+          dropd.innerHTML += "<li><button class=collection onclick=clickME>"+dataElem[i]+"</button></li>";
+        }
+
+
+     });
+    }
+  });
+  
+ return (function(){ return 10;});
+
+}
 /* Using Diffbot and Aylien
 document.addEventListener('DOMContentLoaded', function() {
 getCurrentTabUrl(function(url) {
@@ -83,6 +128,8 @@ getCurrentTabUrl(function(url) {
 
 
 function updateInner(url){
+  addToDictionaryStorage(url);
+  getFromDictonaryStorage();
   var ttURL = 'http://textteaser.com/'
   var summary = ''
   console.log('Summarizing this url: ' + url);
@@ -97,6 +144,19 @@ function updateInner(url){
     
     summary = data;
     console.log(summary);
+    summary["sentences"].sort(function(a,b){
+      var weightA = a["totalScore"];
+      var weightB = b["totalScore"];
+      if(weightA>weightB){
+        return -1;
+      }else if(weightA<weightB){
+        return 1;
+      }else{
+        return 0;
+      }
+    });
+    console.log(summary);
+
   //  display();
   if(summary["sentences"].length > 0){
     if(summary["sentences"].length <5){
@@ -110,23 +170,24 @@ function updateInner(url){
        console.log(summary["sentences"][i]["sentence"])
      }
    }
+   list.innerHTML+="<li><a class=waves-effect waves-light btn href="+url+">Go to Article!</a></li>";
  }else{
   console.log("fuck");
   list.innerHTML +="<div class=row><li>Something went wrong!</li></div><br>"
 }
 
 })
-  .fail(function(data) {
-    if (data.responseJSON == null) {
-      $('#init').empty();
-      $('#init').append('Something wrong happened. Please try again later.');
-    }
-    else {
-      console.log(data.responseJSON.error);
-      $('#init').empty();
-      $('#init').append(data.responseJSON.error);
-    }     
-  });  
+.fail(function(data) {
+  if (data.responseJSON == null) {
+    $('#init').empty();
+    $('#init').append('Something wrong happened. Please try again later.');
+  }
+  else {
+    console.log(data.responseJSON.error);
+    $('#init').empty();
+    $('#init').append(data.responseJSON.error);
+  }     
+});
 }
 function updateExtension(){
   getCurrentTabUrl(function(url){
@@ -134,5 +195,5 @@ function updateExtension(){
   });
 }
 document.addEventListener('DOMContentLoaded', updateExtension);
-//If you want to change what is displayed call updateInner with
+//If you want to change what is displayed call updaeInner with
 //url of what you want.
